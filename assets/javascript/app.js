5f479d99971c8d81ade1731b169e61c992d5393f
JeromeHD <jaymagolan@gmail.com>
2018-11-24 20:18:40 -0500

    trivia1

--- assets/javascript/app.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  // event listeners
-  $("#remaining-time").hide();
-  $("#start").on("click", trivia.startGame);
-  $(document).on("click", ".option", trivia.guessChecker);
-});
